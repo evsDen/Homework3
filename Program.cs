d2249dc527c21c007b995ad237eb6c2a07d14a91
evsDen <saratov642000@gmail.com>
2022-10-07 12:07:19 +0400

    Окончание семинара

--- Program.cs
+++ Program.cs
@@ -23,3 +23,44 @@
 // else
 //     Console.WriteLine("IV");
 
+
+// // Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (Х и У)
+// Console.Clear();
+// Console.Write("Введите четверть: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// while (n > 4)
+// {
+//      Console.WriteLine("Вы ошиблись! Введите номер четверти: ");
+//         n = Convert.ToInt32(Console.ReadLine());
+// }
+// if (n == 1)
+//     Console.WriteLine("x > 0 and y > 0");
+// else if (n == 2)
+//     Console.WriteLine("x < 0 and y > 0");
+// else if (n == 3)
+//     Console.WriteLine("x < 0 and y < 0");
+// else
+//     Console.WriteLine("x > 0 and y < 0");
+
+//  //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.       
+// Console.Clear();
+// Console.Write("Введите первую координату х: ");
+// double x = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите первую координату у: ");
+// double y = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите вторую координату х: ");
+// double x1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите вторую координату у: ");
+// double y1 = Convert.ToDouble(Console.ReadLine());
+// double s =(Math.Round(Math.Sqrt(Math.Pow(x1 - x , 2) + Math.Pow(y1 - y , 2)), 3));
+// Console.WriteLine($"Расстояние между точками: {s}");
+
+
+// // //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= n; i++)
+//     Console.Write($"{i * i} ");
+
+
